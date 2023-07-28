7772693a9f3208be5d9e99b2feff4c47459d285b
Tomasz Brengos <tomasz@genlambda.com>
2023-07-28 23:54:32 +0200

    Add rust configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -69,7 +69,21 @@ return require('packer').startup(function(use)
   use 'justinmk/vim-sneak'
   use 'tiagofumo/vim-nerdtree-syntax-highlight'
   use 'ryanoasis/vim-devicons'
+  use 'neovim/nvim-lspconfig' 
+  use 'simrat39/rust-tools.nvim'
+ -- Completion framework:
+  use 'hrsh7th/nvim-cmp' 
 
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'                             
+  use 'hrsh7th/cmp-path'                              
+  use 'hrsh7th/cmp-buffer'                            
+  use 'hrsh7th/vim-vsnip' 
 end)
 
 
